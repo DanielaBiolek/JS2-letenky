e69387482801c5e89336c998a2ca8db645c7c335
DanielaBiolek <daniela.biolek@centrum.cz>
2025-05-20 15:25:31 +0200

    prvni commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,10 @@
+import { render } from '@czechitas/render';
+import './index.css';
+import { Flight } from '../components/Flight';
+
+document.querySelector('#root').innerHTML = render(
+  <div className="container">
+    <Flight cityFrom='Praha' cityTo="Londýn" timeFrom='3. srpna 2022, 13:30' timeTo='3. srpna 2022, 14:00' />
+    <Flight cityFrom='Praha' cityTo="Londýn" timeFrom='4. srpna 2022, 13:30' timeTo='4. srpna 2022, 14:00' />
+  </div>
+);
